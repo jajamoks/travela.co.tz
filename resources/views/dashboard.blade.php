602fcd33cc761c1b4649ec4e87124348348ea340
JemCdo <jamesemanuel99@gmail.com>
2019-04-11 12:58:18 +0300

    Sidebard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -2,183 +2,11 @@
 
 @section('content')
       <div class="page-wrapper">
-          @include('inc.header')
+          @include('inc.header_mobile')
           @include('inc.sidebar')
 
           <div class="page-container">
-              <!-- HEADER DESKTOP-->
-              <header class="header-desktop">
-                  <div class="section__content section__content--p30">
-                      <div class="container-fluid">
-                          <div class="header-wrap">
-                              <form class="form-header" action="" method="POST">
-                                  <input class="au-input au-input--xl" type="text" name="search" placeholder="Search for datas &amp; reports..." />
-                                  <button class="au-btn--submit" type="submit">
-                                      <i class="zmdi zmdi-search"></i>
-                                  </button>
-                              </form>
-                              <div class="header-button">
-                                  <div class="noti-wrap">
-                                      <div class="noti__item js-item-menu">
-                                          <i class="zmdi zmdi-comment-more"></i>
-                                          <span class="quantity">1</span>
-                                          <div class="mess-dropdown js-dropdown">
-                                              <div class="mess__title">
-                                                  <p>You have 2 news message</p>
-                                              </div>
-                                              <div class="mess__item">
-                                                  <div class="image img-cir img-40">
-                                                      <img src="images/icon/avatar-06.jpg" alt="Michelle Moreno" />
-                                                  </div>
-                                                  <div class="content">
-                                                      <h6>Michelle Moreno</h6>
-                                                      <p>Have sent a photo</p>
-                                                      <span class="time">3 min ago</span>
-                                                  </div>
-                                              </div>
-                                              <div class="mess__item">
-                                                  <div class="image img-cir img-40">
-                                                      <img src="images/icon/avatar-04.jpg" alt="Diane Myers" />
-                                                  </div>
-                                                  <div class="content">
-                                                      <h6>Diane Myers</h6>
-                                                      <p>You are now connected on message</p>
-                                                      <span class="time">Yesterday</span>
-                                                  </div>
-                                              </div>
-                                              <div class="mess__footer">
-                                                  <a href="#">View all messages</a>
-                                              </div>
-                                          </div>
-                                      </div>
-                                      <div class="noti__item js-item-menu">
-                                          <i class="zmdi zmdi-email"></i>
-                                          <span class="quantity">1</span>
-                                          <div class="email-dropdown js-dropdown">
-                                              <div class="email__title">
-                                                  <p>You have 3 New Emails</p>
-                                              </div>
-                                              <div class="email__item">
-                                                  <div class="image img-cir img-40">
-                                                      <img src="images/icon/avatar-06.jpg" alt="Cynthia Harvey" />
-                                                  </div>
-                                                  <div class="content">
-                                                      <p>Meeting about new dashboard...</p>
-                                                      <span>Cynthia Harvey, 3 min ago</span>
-                                                  </div>
-                                              </div>
-                                              <div class="email__item">
-                                                  <div class="image img-cir img-40">
-                                                      <img src="images/icon/avatar-05.jpg" alt="Cynthia Harvey" />
-                                                  </div>
-                                                  <div class="content">
-                                                      <p>Meeting about new dashboard...</p>
-                                                      <span>Cynthia Harvey, Yesterday</span>
-                                                  </div>
-                                              </div>
-                                              <div class="email__item">
-                                                  <div class="image img-cir img-40">
-                                                      <img src="images/icon/avatar-04.jpg" alt="Cynthia Harvey" />
-                                                  </div>
-                                                  <div class="content">
-                                                      <p>Meeting about new dashboard...</p>
-                                                      <span>Cynthia Harvey, April 12,,2018</span>
-                                                  </div>
-                                              </div>
-                                              <div class="email__footer">
-                                                  <a href="#">See all emails</a>
-                                              </div>
-                                          </div>
-                                      </div>
-                                      <div class="noti__item js-item-menu">
-                                          <i class="zmdi zmdi-notifications"></i>
-                                          <span class="quantity">3</span>
-                                          <div class="notifi-dropdown js-dropdown">
-                                              <div class="notifi__title">
-                                                  <p>You have 3 Notifications</p>
-                                              </div>
-                                              <div class="notifi__item">
-                                                  <div class="bg-c1 img-cir img-40">
-                                                      <i class="zmdi zmdi-email-open"></i>
-                                                  </div>
-                                                  <div class="content">
-                                                      <p>You got a email notification</p>
-                                                      <span class="date">April 12, 2018 06:50</span>
-                                                  </div>
-                                              </div>
-                                              <div class="notifi__item">
-                                                  <div class="bg-c2 img-cir img-40">
-                                                      <i class="zmdi zmdi-account-box"></i>
-                                                  </div>
-                                                  <div class="content">
-                                                      <p>Your account has been blocked</p>
-                                                      <span class="date">April 12, 2018 06:50</span>
-                                                  </div>
-                                              </div>
-                                              <div class="notifi__item">
-                                                  <div class="bg-c3 img-cir img-40">
-                                                      <i class="zmdi zmdi-file-text"></i>
-                                                  </div>
-                                                  <div class="content">
-                                                      <p>You got a new file</p>
-                                                      <span class="date">April 12, 2018 06:50</span>
-                                                  </div>
-                                              </div>
-                                              <div class="notifi__footer">
-                                                  <a href="#">All notifications</a>
-                                              </div>
-                                          </div>
-                                      </div>
-                                  </div>
-                                  <div class="account-wrap">
-                                      <div class="account-item clearfix js-item-menu">
-                                          <div class="image">
-                                              <img src="images/icon/avatar-01.jpg" alt="John Doe" />
-                                          </div>
-                                          <div class="content">
-                                              <a class="js-acc-btn" href="#">john doe</a>
-                                          </div>
-                                          <div class="account-dropdown js-dropdown">
-                                              <div class="info clearfix">
-                                                  <div class="image">
-                                                      <a href="#">
-                                                          <img src="images/icon/avatar-01.jpg" alt="John Doe" />
-                                                      </a>
-                                                  </div>
-                                                  <div class="content">
-                                                      <h5 class="name">
-                                                          <a href="#">john doe</a>
-                                                      </h5>
-                                                      <span class="email">johndoe@example.com</span>
-                                                  </div>
-                                              </div>
-                                              <div class="account-dropdown__body">
-                                                  <div class="account-dropdown__item">
-                                                      <a href="#">
-                                                          <i class="zmdi zmdi-account"></i>Account</a>
-                                                  </div>
-                                                  <div class="account-dropdown__item">
-                                                      <a href="#">
-                                                          <i class="zmdi zmdi-settings"></i>Setting</a>
-                                                  </div>
-                                                  <div class="account-dropdown__item">
-                                                      <a href="#">
-                                                          <i class="zmdi zmdi-money-box"></i>Billing</a>
-                                                  </div>
-                                              </div>
-                                              <div class="account-dropdown__footer">
-                                                  <a href="#">
-                                                      <i class="zmdi zmdi-power"></i>Logout</a>
-                                              </div>
-                                          </div>
-                                      </div>
-                                  </div>
-                              </div>
-                          </div>
-                      </div>
-                  </div>
-              </header>
-              <!-- HEADER DESKTOP-->
+              @include('inc.header_desktop')
 
               <!-- MAIN CONTENT-->
               <div class="main-content">
@@ -187,12 +15,11 @@
                           <div class="row">
                               <div class="col-md-12">
                                   <div class="overview-wrap">
-                                      <h2 class="title-1">overview</h2>
-                                      <button class="au-btn au-btn-icon au-btn--blue">
-                                          <i class="zmdi zmdi-plus"></i>add item</button>
+                                      <h2 class="title-1">Dashboard</h2>
                                   </div>
                               </div>
                           </div>
+
                           <div class="row m-t-25">
                               <div class="col-sm-6 col-lg-3">
                                   <div class="overview-item overview-item--c1">
@@ -202,34 +29,30 @@
                                                   <i class="zmdi zmdi-account-o"></i>
                                               </div>
                                               <div class="text">
-                                                  <h2>10368</h2>
-                                                  <span>members online</span>
+                                                  <h2>36</h2>
+                                                  <span>Total Agents</span>
                                               </div>
-                                          </div>
-                                          <div class="overview-chart">
-                                              <canvas id="widgetChart1"></canvas>
                                           </div>
                                       </div>
                                   </div>
                               </div>
+
                               <div class="col-sm-6 col-lg-3">
                                   <div class="overview-item overview-item--c2">
                                       <div class="overview__inner">
                                           <div class="overview-box clearfix">
                                               <div class="icon">
-                                                  <i class="zmdi zmdi-shopping-cart"></i>
+                                                  <i class="zmdi zmdi-bus"></i>
                                               </div>
                                               <div class="text">
-                                                  <h2>388,688</h2>
-                                                  <span>items solid</span>
+                                                  <h2>18</h2>
+                                                  <span>Total Busses</span>
                                               </div>
-                                          </div>
-                                          <div class="overview-chart">
-                                              <canvas id="widgetChart2"></canvas>
                                           </div>
                                       </div>
                                   </div>
                               </div>
+
                               <div class="col-sm-6 col-lg-3">
                                   <div class="overview-item overview-item--c3">
                                       <div class="overview__inner">
@@ -238,16 +61,14 @@
                                                   <i class="zmdi zmdi-calendar-note"></i>
                                               </div>
                                               <div class="text">
-                                                  <h2>1,086</h2>
-                                                  <span>this week</span>
+                                                  <h2>486</h2>
+                                                  <span>Ticket Booked</span>
                                               </div>
-                                          </div>
-                                          <div class="overview-chart">
-                                              <canvas id="widgetChart3"></canvas>
                                           </div>
                                       </div>
                                   </div>
                               </div>
+
                               <div class="col-sm-6 col-lg-3">
                                   <div class="overview-item overview-item--c4">
                                       <div class="overview__inner">
@@ -256,12 +77,9 @@
                                                   <i class="zmdi zmdi-money"></i>
                                               </div>
                                               <div class="text">
-                                                  <h2>$1,060,386</h2>
-                                                  <span>total earnings</span>
+                                                  <h2> 947,060</h2>
+                                                  <span>Total Earnings</span>
                                               </div>
-                                          </div>
-                                          <div class="overview-chart">
-                                              <canvas id="widgetChart4"></canvas>
                                           </div>
                                       </div>
                                   </div>
@@ -270,7 +88,7 @@
 
                           <div class="row">
                               <div class="col-lg-12">
-                                  <h2 class="title-1 m-b-25">Earnings By Items</h2>
+                                  <h2 class="title-1 m-b-25">Recently Ticket Booked</h2>
                                   <div class="table-responsive table--no-card m-b-40">
                                       <table class="table table-borderless table-striped table-earning">
                                           <thead>
@@ -354,14 +172,14 @@
                               </div>
 
                           </div>
-                          
-                          <div class="row">
+
+                          {{-- <div class="row">
                               <div class="col-md-12">
                                   <div class="copyright">
-                                      <p>Copyright © 2018 Colorlib. All rights reserved. Template by <a href="https://colorlib.com">Colorlib</a>.</p>
+                                      <p>Copyright © 2019 . All rights reserved. Template by <a href="https://colorlib.com">Colorlib</a>.</p>
                                   </div>
                               </div>
-                          </div>
+                          </div> --}}
                       </div>
                   </div>
               </div>
