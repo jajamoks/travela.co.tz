0dfdf66586b14eae269d8bbd0cf16f69cb99e429
JemCdo <jamesemanuel99@gmail.com>
2019-05-27 13:09:21 +0300

    Mobile Menu

--- resources/views/inc/headerMobile.blade.php
+++ resources/views/inc/headerMobile.blade.php
@@ -2,9 +2,8 @@
     <div class="header-mobile__bar">
         <div class="container-fluid">
             <div class="header-mobile-inner">
-                <a class="logo" href="{{ url('/') }}">
-                    BTS
-                    <!-- <img src="images/icon/logo.png" alt="CoolAdmin" /> -->
+                <a class="logo text-uppercase" href="{{ url('/') }}">
+                    <h4>Online Bus Ticketing System</h4>
                 </a>
                 <button class="hamburger hamburger--slider" type="button">
                     <span class="hamburger-box">
@@ -17,98 +16,87 @@
     <nav class="navbar-mobile">
         <div class="container-fluid">
             <ul class="navbar-mobile__list list-unstyled">
+                <li class="active has-sub">
+                    <a class="js-arrow" href="/dashboard">
+                        <i class="fas fa-tachometer-alt"></i>Dashboard
+                    </a>
+                </li>
+
+                <li>
+                    <a href="/admin/passengers"><i class="fas fa-users"></i>Passengers</a>
+                </li>
+
                 <li class="has-sub">
                     <a class="js-arrow" href="#">
-                        <i class="fas fa-tachometer-alt"></i>Dashboard</a>
+                        <i class="fas fa-file-alt"></i>Tickets
+                    </a>
                     <ul class="navbar-mobile-sub__list list-unstyled js-sub-list">
                         <li>
-                            <a href="index.html">Dashboard 1</a>
+                            <a href="/admin/tickets/newTicket"></i>New Ticket</a>
                         </li>
                         <li>
-                            <a href="index2.html">Dashboard 2</a>
-                        </li>
-                        <li>
-                            <a href="index3.html">Dashboard 3</a>
-                        </li>
-                        <li>
-                            <a href="index4.html">Dashboard 4</a>
+                            <a href="/admin/tickets"></i>All Tickets</a>
                         </li>
                     </ul>
                 </li>
-                <li>
-                    <a href="#">
-                        <i class="fas fa-chart-bar"></i>Charts</a>
-                </li>
-                <li>
-                    <a href="#">
-                        <i class="fas fa-table"></i>Tables</a>
-                </li>
-                <li>
-                    <a href="form.html">
-                        <i class="far fa-check-square"></i>Forms</a>
-                </li>
-                <li>
-                    <a href="#">
-                        <i class="fas fa-calendar-alt"></i>Calendar</a>
-                </li>
-                <li>
-                    <a href="map.html">
-                        <i class="fas fa-map-marker-alt"></i>Maps</a>
-                </li>
+
+                @if (Auth::user()->hasRole('Admin')==1)
                 <li class="has-sub">
                     <a class="js-arrow" href="#">
-                        <i class="fas fa-copy"></i>Pages</a>
+                        <i class="fas fa-user"></i>Staffs
+                    </a>
                     <ul class="navbar-mobile-sub__list list-unstyled js-sub-list">
                         <li>
-                            <a href="login.html">Login</a>
+                            <a href="/admin/staffs/addStaff"></i>Add new</a>
                         </li>
                         <li>
-                            <a href="register.html">Register</a>
-                        </li>
-                        <li>
-                            <a href="forget-pass.html">Forget Password</a>
+                            <a href="/admin/staffs"></i>View All</a>
                         </li>
                     </ul>
                 </li>
+
                 <li class="has-sub">
                     <a class="js-arrow" href="#">
-                        <i class="fas fa-desktop"></i>UI Elements</a>
+                        <i class="fas fa-bus"></i>Bus Management
+                    </a>
                     <ul class="navbar-mobile-sub__list list-unstyled js-sub-list">
                         <li>
-                            <a href="button.html">Button</a>
+                            <a href="/admin/busses/addBus"></i>Add new</a>
                         </li>
                         <li>
-                            <a href="badge.html">Badges</a>
-                        </li>
-                        <li>
-                            <a href="tab.html">Tabs</a>
-                        </li>
-                        <li>
-                            <a href="card.html">Cards</a>
-                        </li>
-                        <li>
-                            <a href="alert.html">Alerts</a>
-                        </li>
-                        <li>
-                            <a href="progress-bar.html">Progress Bars</a>
-                        </li>
-                        <li>
-                            <a href="modal.html">Modals</a>
-                        </li>
-                        <li>
-                            <a href="switch.html">Switchs</a>
-                        </li>
-                        <li>
-                            <a href="grid.html">Grids</a>
-                        </li>
-                        <li>
-                            <a href="fontawesome.html">Fontawesome Icon</a>
-                        </li>
-                        <li>
-                            <a href="typo.html">Typography</a>
+                            <a href="/admin/busses"></i>View All</a>
                         </li>
                     </ul>
                 </li>
+
+                <li class="has-sub">
+                    <a class="js-arrow" href="#">
+                        <i class="fas fa-road"></i>Route Details
+                    </a>
+                    <ul class="navbar-mobile-sub__list list-unstyled js-sub-list">
+                        <li>
+                            <a href="/admin/routes/addRoute">Add new</a>
+                        </li>
+                        <li>
+                            <a href="/admin/routes">View All</a>
+                        </li>
+                    </ul>
+                </li>
+
+                <li class="has-sub">
+                    <a class="js-arrow" href="#">
+                        <i class="fas fa-user"></i>Schedule Trip
+                    </a>
+                    <ul class="navbar-mobile-sub__list list-unstyled js-sub-list">
+                        <li>
+                            <a href="/admin/schedule/scheduleTrip"></i>Add new</a>
+                        </li>
+                        <li>
+                            <a href="/admin/schedule"></i>View All</a>
+                        </li>
+                    </ul>
+                </li>
+                @endif
             </ul>
         </div>
     </nav>
